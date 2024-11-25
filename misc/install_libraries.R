c79ecd4a376220303484f117c640ea52e8c22d1a
Vincent Rouilly <vincent.rouilly@datactix.com>
2024-11-25 17:59:18 +0000

    list of required libraries for workshop

---
+++ misc/install_libraries.R
@@ -0,0 +1,23 @@
+# Install libraries
+
+# Data Wrangling
+install.packages("tidyverse")
+install.packages("writexl")
+install.packages("here")
+
+# Data Viz
+# OK
+
+# PCA
+install.packages("corrplot")
+install.packages("GGally")
+install.packages("FactoMineR")
+install.packages("factoextra")
+
+# NHST and Linear Models
+install.packages("ggsignif")
+
+# BioC limma
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install("limma")
